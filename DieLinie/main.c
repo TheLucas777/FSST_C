b8b25b4a42275a105bd305766106e2aae79b97ff
TheLucas777 <64582502+TheLucas777@users.noreply.github.com>
2022-11-23 11:58:06 +0100

    adding HÜ

--- DieLinie/main.c
+++ DieLinie/main.c
@@ -2,20 +2,33 @@
 #include "BildschirmRoutinen.h"
 
 // Erstellt eine Horizontale linie
-void HLinie(int iZeile, int iStartSpalte, int iEndeSpalte, char cZeichen)
-{
+void HLinie(int iZeile, int iStartSpalte, int iEndeSpalte, char cZeichen) {
     int i;
     GotoZS(iZeile, iStartSpalte);
-    for (i = iStartSpalte; i <= iEndeSpalte; i++)
-    {
+    for (i = iStartSpalte; i <= iEndeSpalte; i++) {
         printf("%c", cZeichen);
     }
 }
 
+// Erstellt eine Vertikale linie
+void VLinie(int iStartZeile, int iEndeZeile, int iSpalte, char cZeichen) {
+    int i;
+    for (i = iStartZeile; i <= iEndeZeile; i++) {
+        GotoZS(i, iSpalte);
+        printf("%c", cZeichen);
+    }
+}
+
+// Erstellt eine Rechteck
+void Rechteck(int iStartZeile, int iStartSpalte, int iEndeZeile, int iEndeSpalte, char cZeichen) {
+    HLinie(iStartZeile, iStartSpalte, iEndeSpalte, cZeichen);
+    HLinie(iEndeZeile, iStartSpalte, iEndeSpalte, cZeichen);
+    VLinie(iStartZeile, iEndeZeile, iStartSpalte, cZeichen);
+    VLinie(iStartZeile, iEndeZeile, iEndeSpalte, cZeichen);
+}
 
 
 int main() {
-    HLinie(3, 3, 70, '#');
-    HLinie(20, 10, 70, '#');
+    Rechteck(5, 5, 10, 20, '*');
     return 0;
 }
